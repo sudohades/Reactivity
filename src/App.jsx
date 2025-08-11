63e248a3966c8c8294ecd1fa8eb0f3214b6eb7a3
HVDES-808 <jkiprutto63003@anu.ac.ke>
2025-08-11 23:42:18 +0300

    minor minor changes, possible chances of a breaking commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,26 +10,16 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class='header'>
+        <div>
+          <ul class="navButtons">
+            <li>HOME</li>
+            <li>DOMAIN__EXPANSION</li>
+            <li>HVDES:~ touch<any id="blinking">_</any></li>
+          </ul>
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1) && console.log('Button clicked ' + (count + 1) +' times.')}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 
