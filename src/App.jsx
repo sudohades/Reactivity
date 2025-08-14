0149e757de42c1a7efa946162e85d460486eca1f
HVDES-808 <jkiprutto63003@anu.ac.ke>
2025-08-14 21:12:05 +0300

    updated some files

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,10 @@ import './App.css'
 function App() {
   return (
     <>
-    
+    <div className="hero">
+      <h1> J E S S E</h1>
+      <h2>WELCOME TO MY DOMAIN</h2>
+    </div>
     </>
   )
 
