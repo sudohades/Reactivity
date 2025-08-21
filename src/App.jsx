9207c2724c21099b2f42359fab722b1ce8e46d3e
Jesse Jacob <jesse.jacob.808@gmail.com>
2025-08-21 04:04:00 +0300

    working on an alternate login page with some beautiful design and implementation. Really getting a hang of this

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './newComponents/Navbar/Navbar';
 import Home from './newComponents/Focus/Home';
 import Blog from './newComponents/Focus/Blog';
 import Contact from './newComponents/Focus/Contact';
+import Home3 from './pages/Home3';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <div className="app">
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/' element={<Home3 />} />
           <Route path='blog' element={<Blog />} />
           <Route path='contact' element={<Contact />} />
         </Routes>
