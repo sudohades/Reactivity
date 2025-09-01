d803cf6a2ae8276798c5c241a8dd645cf9cc86fa
Jesse Jacob <jesse.jacob.808@gmail.com>
2025-09-01 03:01:21 +0300

    bout to edit contact page

--- src/newComponents/Focus/Contact.jsx
+++ src/newComponents/Focus/Contact.jsx
@@ -3,12 +3,13 @@ import "./Contact.css";
 import { useState} from 'react';
 
 function Contact(){
-
+    
 
 };
 
-//to fill up the page, side project to have a live metrics dashboard for X
-// This will involve creating a new component that fetches and displays data in real-time.
+// to fill up the page, side project to have a live metrics dashboard for X
+// main point is leading to X profile 
+// This will involve creating a new component that fetches and displays data from an API.
 
 
 export default Contact;
